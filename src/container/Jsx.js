1ad02c1022f0f114997ee12b9cb099c446b7ae7a
akhil <akhildsvvbca@gmail.com>
2024-04-07 15:28:55 +0530

    update1

--- src/container/Jsx.js
+++ src/container/Jsx.js
@@ -13,7 +13,67 @@ function CheckEvenOrOddBySwitch(num){
 function CheckEvenOrOdd(num){
 if (num % 2 == 0){
   return <div>Even</div>
+}let name='Ram';
+function CheckEvenOrOdd(num){
+  if(num%2===0){
+     return <div>Even</div>
+  }
+  else{
+    return <div>Odd</div>
+  }
 }
+  function CheckEvenOddBySwitch(num){
+  switch(num3%2)
+  {
+    case 0:
+      return <div>Even</div>
+      break;
+    case 1:
+      return <div>Odd</div>
+  }
+}
+const users=[
+  { name:'Ram',age:32},
+  { name:'Sham',age:22},
+  { name:'Mohan',age:25},
+  { name:'Pratap',age:24}
+]
+
+let num1=17;
+let num2=8
+let num3=67;
+function Jsx() {
+  return (
+    <div>
+      <h2>Hello {name}</h2>
+      <p></p>
+      <div>
+        {
+        CheckEvenOrOdd(10)
+        } 
+        {
+          num1%2===0?<div>Even</div>:<div>Odd</div>
+        }
+        {
+          num2%2===0&&<div>Even</div>
+        }
+        <h4>List of peoples</h4>
+        {
+          users.map((item,index)=> {return<div key={index}>{item.name}-{item.age}</div>})
+        }
+        <h4>Switch case</h4>
+        {
+          CheckEvenOddBySwitch(num3)
+        }
+        {
+          
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Jsx;
   else {
          return <div>Odd</div>
   }
--- src/container/Users.js
+++ src/container/Users.js
@@ -1,10 +1,13 @@
-import React , {useState} from "react"
+import React , {useEffect, useState} from "react"
 function Users(){
-    const [users,setUsers]= useState(
-    [
-        {name:"Mohan",age:25},
-        {name:"Sohan",age:26}
-    ])
+    const [users,setUsers]= useState([])
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(data=>{
+            setUsers(data)
+        })
+    }),[]}
     return(
         <div>
             <h2>Users</h2>
